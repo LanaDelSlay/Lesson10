88f2278ccb6a2c3be3bd40dc7d1dee71d6b7e4de
LanaDelSlay <gmiller1902@gmail.com>
2020-04-05 20:12:06 -0700

    Added phone number + email verifiers!

---
+++ src/exercises/MailAndNumber.java
@@ -0,0 +1,57 @@
+package exercises;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import examples.FileHelper;
+
+public class MailAndNumber {
+
+	List<String> tlds = FileHelper.loadFileContentsIntoArrayList("resource/tlds.txt");
+	
+	public boolean verifyMail(String email) {
+		
+		if (email.length() >= 6) { 
+			//Short email could be a@g.cn
+			if(email.contains("@")) { 
+				if(legitDomain(email)) {
+					return true;
+				}
+			}
+		}
+		
+		return false;
+		
+	}
+	
+	public boolean legitDomain(String email) {
+		int myAt = email.indexOf("@");
+		String emailIntersection = email.substring(myAt, email.length());
+		int myDomain = emailIntersection.indexOf(".");
+		String domainExtension = emailIntersection.substring(myDomain, emailIntersection.length());
+		
+		for (String s: tlds) {
+			if (domainExtension.contains(s.toLowerCase())) {
+				System.out.println(s);
+				return true;
+			}
+		}  return false;
+		
+	}
+	
+	Pattern pattern = Pattern.compile("^\\(?([0-9]{3})\\)?[-.\\s]?([0-9]{3})[-.\\s]?([0-9]{4})$");
+	
+	public boolean verifyNumber(String number) {
+		Matcher matcher = pattern.matcher(number);
+		if(matcher.matches()) {
+			return true;
+		} else return false;
+	}
+	
+	public static void main(String[] args) {
+		MailAndNumber obj = new MailAndNumber();
+		System.out.println(obj.legitDomain("admin@fbi.ov"));
+	}
+
+}
---
+++ test/exercises/PhoneNumberTest.java
@@ -0,0 +1,29 @@
+package exercises;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+ class PhoneNumberTest {
+
+	 MailAndNumber mnn = new MailAndNumber();
+	 
+		@Test 
+		void testPhone() {
+			String phonebook[]={"619 3453453", "619278-6547", "619-569-4321", "(568) 456-3241" , "789562 3256" };
+			for (String n: phonebook) {
+			assertTrue(mnn.verifyNumber(n));
+			}
+		}
+		
+		@Test
+		void testMail() {
+			String emails[] = {"gmiller1902@gmail.com", "admin@fbi.ov", "random@website.net", "snoopdog@cox.edu"};
+			for (String s: emails) {
+				assertTrue(mnn.verifyMail(s));
+			}
+		}
+
+
+}
