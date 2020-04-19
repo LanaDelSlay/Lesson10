3272b989c93314fe5058e386897b9d3085fbcbd9
Graham <gmiller1902@gmail.com>
2020-04-19 14:26:47 -0700

    Added checks for special characters in list and for selected puzzle from list!

--- src/exercises/Hangman.java
+++ src/exercises/Hangman.java
@@ -20,23 +20,38 @@ import examples.FileHelper;
 public class Hangman extends KeyAdapter {
 
 	Stack<String> puzzles = new Stack<String>();
-	List<String> words = FileHelper.loadFileContentsIntoArrayList("resource/words.txt");
+	static List<String> words = FileHelper.loadFileContentsIntoArrayList("resource/words.txt");
 	ArrayList<JLabel> boxes = new ArrayList<JLabel>();
 	int lives = 9;
 	JLabel livesLabel = new JLabel("" + lives);
+	static String[] specChars = {"!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@", "\\", "[", "]", "^", "_", "`", "{", "|", "}" , "~"};
 
 	public static void main(String[] args) {
 		Hangman hangman = new Hangman();
-		hangman.addPuzzles();
+		hangman.checkWordList();
 		hangman.addPuzzles();
 		hangman.createUI();
 	}
 
 	private void addPuzzles() {
-		puzzles.add(getRandomWord(words));
-		getRandomWord(words);
+		puzzles.add(getRandomWord());
+		
 	}
 
+	
+	public static void checkWordList () {
+		for (String myWords : words) {
+			for (String badChars : specChars) {				
+				if (myWords.contains(badChars)) {
+					System.out.println("Special Char detected in list");
+					System.out.println("\"" + badChars + "\" somewhere in list");
+				}
+				
+				}
+			}
+
+		}
+	
 	
 	JPanel panel = new JPanel();
 	private String puzzle;
@@ -58,12 +73,20 @@ public class Hangman extends KeyAdapter {
 		lives = 9;
 		addPuzzles();
 		livesLabel.setText("" + lives);
-		puzzle = puzzles.pop();
+		puzzle = puzzles.pop();		
 		System.out.println("puzzle is now " + puzzle);
 		createBoxes();
 	}
-
+	
+	
 	public void keyTyped(KeyEvent arg0) {
+		String input = String.valueOf(arg0.getKeyChar());
+		for (int  i = 0; i <= specChars.length - 1; i++) {
+			if (input.equals(specChars[i])) {
+				throw new RuntimeException("You typed a special char! Congrats!!!");
+			}
+		}
+		
 		System.out.println(arg0.getKeyChar());
 		updateBoxesWithUserInput(arg0.getKeyChar());
 		if (lives == 0) {
@@ -102,10 +125,20 @@ public class Hangman extends KeyAdapter {
 		boxes.clear();
 	}
 	
-	public String getRandomWord(List<String> words) 
+	public String getRandomWord() 
     { 
-        Random rand = new Random(); 
-        return words.get(rand.nextInt(words.size())); 
+       Random rand = new Random(); 
+       String newWord = words.get(rand.nextInt(words.size()));
+       
+			for (String badChars : specChars) {				
+				if (newWord.contains(badChars)) {
+					System.out.println("\"" + badChars + "\" attempted to be set as puzzle");
+					return getRandomWord();
+				} 				
+				}
+
+	return newWord;
+    
     } 
 	
 	public void playDeathKnell() {
